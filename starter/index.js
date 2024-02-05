24624804cabf2471c51d6c35a5f72a54714b22bd
Ioana Iosub <stu.waltham@gmail.com>
2024-02-05 17:22:39 +0000

    Validate to check if user provided a value.

--- starter/index.js
+++ starter/index.js
@@ -9,50 +9,112 @@ const questions = [
     type: "input",
     name: "title",
     message: "What is the title of your project?",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "Enter a value to continue!";
+      }
+    },
   },
   {
     type: "input",
     name: "description",
-    message:
-      "Provide a short description explaining the what, why, and how of your project.",
+    message: "Provide a short description of your project.",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "Enter a value to continue!";
+      }
+    },
   },
   {
     type: "input",
     name: "installation",
     message: "What are the steps required to install your project?",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "Enter a value to continue!";
+      }
+    },
   },
   {
     type: "input",
     name: "usage",
     message:
       "Provide instructions and examples for use. Include screenshots as needed.",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "Enter a value to continue!";
+      }
+    },
   },
   {
     type: "list",
     name: "license",
     message: "Choose a license for your project:",
     choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"],
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "Enter a value to continue!";
+      }
+    },
   },
   {
     type: "input",
     name: "contributing",
     message:
       "If you would like other developers to contribute, please enter your guidelines for how to do so.",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "Enter a value to continue!";
+      }
+    },
   },
   {
     type: "input",
     name: "tests",
     message: "Provide examples on how to run tests for your application.",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "Enter a value to continue!";
+      }
+    },
   },
   {
     type: "input",
     name: "github",
     message: "Enter your GitHub Username:",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "Enter a value to continue!";
+      }
+    },
   },
   {
     type: "input",
     name: "email",
     message: "Enter your email address:",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "Enter a value to continue!";
+      }
+    },
   },
 ];
 
